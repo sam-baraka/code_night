2cbc391f834e528db57a00b41229e4e77977f209
Samuel Baraka <sababuvercetti@gmail.com>
2023-03-04 02:54:02 +0300

    test: Updated tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:code_night/app/app.dart';
-import 'package:code_night/counter/counter.dart';
+import 'package:code_night/app/survery/view/survey_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders Survey Page', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(SurveyPage), findsOneWidget);
     });
   });
 }
