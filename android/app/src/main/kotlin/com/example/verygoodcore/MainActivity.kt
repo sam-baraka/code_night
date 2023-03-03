dab958b705d0e76190d1f258582d36da5b547ef4
Samuel Baraka <sababuvercetti@gmail.com>
2023-03-03 23:45:06 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.code_night
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:code_night/app/app.dart';
+import 'package:code_night/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
